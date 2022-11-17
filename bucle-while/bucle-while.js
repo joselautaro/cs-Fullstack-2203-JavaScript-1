baee8d8007f08b69de0c36f7c148ec45d8181b87
Lautaro Montaña <joselautarom@gmail.com>
2022-11-17 20:00:19 -0300

    [FEATURE] Aprendemos el bucle while

---
+++ bucle-while/bucle-while.js
@@ -0,0 +1,17 @@
+//Primero declaramos el counter
+
+let contador = 11;
+
+//Después escribimos la palabra reservada while
+
+//Entonces decims, MIENTRAS que, la condicion sea menor o igual que el valor de x, en este caso se va sumando una vuelta mas hasta que se llega al valor a ejecutar dentro de la condicion
+
+while( contador <= 10){
+
+    // Backticks
+
+    console.log(`Iteracion ${contador}`)
+
+    contador++
+
+}
