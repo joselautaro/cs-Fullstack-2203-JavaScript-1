962f99d057d5d3fd54d26c3c1f7a410b02a33384
Lautaro Montaña <joselautarom@gmail.com>
2022-11-17 20:15:37 -0300

    [FEATURE] Aprendemos do-while

---
+++ do-while/do-while.js
@@ -0,0 +1,15 @@
+
+//haz esto mientras se cumpla la condición
+
+let y = 0.5;
+
+do{
+
+
+    console.log('Iteración ' + y)
+
+    y++
+
+    //Es muy importante la diferencia entre el while y el do while, y es la que en do while condición se ejecuta al menos una vez
+
+}while(y <= 10)
