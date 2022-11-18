87758f630edadf2ed2633eb638a3bb82f6d12173
Lautaro Montaña <joselautarom@gmail.com>
2022-11-17 21:02:56 -0300

    [FEATURE] Aprendemos objetos

---
+++ objetos/objetos.js
@@ -0,0 +1,19 @@
+const persona = {
+    //Clave | Valor
+    nombre: "Lucia",
+    apellido: "Disandro",
+    edad: 27,
+    sabeProgramar: true,
+    colorFavorito: "celeste"
+}
+
+console.log(persona)
+
+console.log(persona.nombre)
+
+
+// dot.notation
+persona.nombre = "Lautaro"
+
+//bracket.notation
+persona["colorFavorito"] = "Verde"
