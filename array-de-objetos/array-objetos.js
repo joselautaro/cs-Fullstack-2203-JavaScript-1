a8237818d08c5ae8bde56b066cb19d1e085a73ec
Lautaro Montaña <joselautarom@gmail.com>
2022-11-17 21:21:05 -0300

    [FEATURE] Aprendemos array de objetos

---
+++ array-de-objetos/array-objetos.js
@@ -0,0 +1,21 @@
+const personas = [
+  {
+    nombre: "Lucia",
+    apellido: "Disandro",
+    edad: 27,
+    sabeProgramar: true,
+    colorFavorito: ['celeste', 'azul'],
+    img: 'img/imagen.jpg'
+  },
+  {
+    nombre: "Lautaro",
+    apellido: "Montaña",
+    edad: 30,
+    sabeProgramar: false,
+    colorFavorito: "rojo",
+  },
+  'talle',
+  'medida',
+];
+
+console.log(personas[0])
