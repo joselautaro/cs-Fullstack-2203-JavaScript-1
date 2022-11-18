dbf28137daa0428faef93169cb91a57561bbc595
Lautaro Montaña <joselautarom@gmail.com>
2022-11-17 21:44:33 -0300

    [FEATURE] Vemos funciones básicas

---
+++ funciones-basicas/funciones.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingresar nombre");
+
+function saludar (nombre){
+    document.write("Te damos la bienvenida " + nombre + `<br>` + `<h1>Hola Mundo</h1>` + `<br>`)
+    document.write("Te damos la bienvenida " + nombre)
+}
+
+saludar(nombre)
